b801a7ee956793698716eb50da6a687979e24f58
Milkoeee <emile38380@gmail.com>
2023-11-30 19:46:25 +0100

    Ajout d'une animation, et organisation des dossiers

--- Scripts/Character/player.gd
+++ Scripts/Character/player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
+
+var down = load("res://assets/Sprites/player_down.png")
+var up = load("res://assets/Sprites/player_up.png")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	player_movement(delta)
 	
+
 
 func player_movement(delta) :
 	#print(_on_body_entered(body))
@@ -12,26 +17,15 @@ func player_movement(delta) :
 	if Input.is_action_pressed("ui_left") :
 		global_position.x -= 10
 	if Input.is_action_pressed("ui_up") :
+		$Sprite.texture=up
 		global_position.y -= 10
 	if Input.is_action_pressed("ui_down") :
+		$Sprite.texture=down
 		global_position.y += 10
 	
 	if velocity.length() > 50000 * delta :
 		velocity -= velocity.normalized() * 50000 * delta
 	else :
 		velocity = Vector2.ZERO
-		
 	move_and_slide()
 
-func _on_body_entered(body):
-	if body.name == "Arrow_Up" :
-		return "up"
-		
-	if body.name == "Arrow_Down" :
-		return "down"
-	
-	if body.name == "Arrow_Left":
-		return "left"
-		
-	if body.name == "Arrow_Right" :
-		return "right"
