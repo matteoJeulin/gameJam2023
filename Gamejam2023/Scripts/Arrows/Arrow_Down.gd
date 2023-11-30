59e866e2abd041eec994e9ca870cb983a2aef788
LeandreLeNulzec <152444817+LeandreLeNulzec@users.noreply.github.com>
2023-11-30 23:16:34 +0100

    Add files via upload

---
+++ Gamejam2023/Scripts/Arrows/Arrow_Down.gd
@@ -0,0 +1,12 @@
+extends Area2D
+ 
+var accel = 5000
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player" :
+		body.velocity.y = accel
