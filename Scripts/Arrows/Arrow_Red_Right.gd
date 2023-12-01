bdf758836e9445d2a5661a4eaa9891212fe4f27c
Milkoeee <emile38380@gmail.com>
2023-12-01 21:52:15 +0100

    Added more arrows, and changed NPC behaviour

---
+++ Scripts/Arrows/Arrow_Red_Right.gd
@@ -0,0 +1,16 @@
+extends Area2D
+ 
+var accel = 4000
+
+func _ready() :
+	$Sprite2D.texture = load("res://assets/sprites/arrows/arrow_right_red_" + str(randi_range(1,4)) +".png")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(_delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player" :
+		body.velocity = Vector2.ZERO
+		body.velocity.x = accel
